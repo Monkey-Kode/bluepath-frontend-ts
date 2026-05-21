427d07c7bbcdecaaa9e907a9bd5b73f1bc746dcb
Jull Weber <jull@monkeykode.com>
2026-05-20 20:04:31 -0700

    style(contact): prettier reformat

--- components/ContactBody.tsx
+++ components/ContactBody.tsx
@@ -7,8 +7,8 @@ import { loader } from '@/lib/mapsLoader';
 import sortObject from '@/utils/sortObject';
 import splitByNewLines from '@/utils/splitByNewLines';
 import type {
-  AddressesQueryResult,
-  PageBySlugQueryResult,
+    AddressesQueryResult,
+    PageBySlugQueryResult,
 } from '@/sanity.types';
 
 type Page = NonNullable<PageBySlugQueryResult>;
@@ -16,82 +16,82 @@ type Page = NonNullable<PageBySlugQueryResult>;
 const emailLink = '/bluepath-email-link.png';
 
 export const ContactBody = ({
-  id,
-  Heading,
-  name,
-  richcontent,
-  addresses,
+    id,
+    Heading,
+    name,
+    richcontent,
+    addresses,
 }: {
-  id: Page['id'];
-  Heading: Page['Heading'];
-  name: Page['name'];
-  richcontent: Page['richcontent'];
-  addresses: AddressesQueryResult;
+    id: Page['id'];
+    Heading: Page['Heading'];
+    name: Page['name'];
+    richcontent: Page['richcontent'];
+    addresses: AddressesQueryResult;
 }) => {
-  const orderedAddresses = sortObject(addresses);
-  const [showInfo, setShowInfo] = useState(false);
+    const orderedAddresses = sortObject(addresses);
+    const [showInfo, setShowInfo] = useState(false);
 
-  useEffect(() => {
-    loader.load().then(() => {
-      const googleMaps = window.google.maps;
-      const map: Record<number, google.maps.Map> = [];
+    useEffect(() => {
+        loader.load().then(() => {
+            const googleMaps = window.google.maps;
+            const map: Record<number, google.maps.Map> = [];
 
-      orderedAddresses.forEach(({ _id, location: center }) => {
-        if (googleMaps && document.getElementById(String(_id)) !== null) {
-          map[Number(_id)] = new window.google.maps.Map(
-            document.getElementById(String(_id)) as HTMLElement,
-            {
-              center: center as unknown as google.maps.LatLngLiteral,
-              zoom: 12,
-            },
-          );
-        }
-      });
-    });
-  }, [orderedAddresses]);
+            orderedAddresses.forEach(({ _id, location: center }) => {
+                if (googleMaps && document.getElementById(String(_id)) !== null) {
+                    map[Number(_id)] = new window.google.maps.Map(
+                        document.getElementById(String(_id)) as HTMLElement,
+                        {
+                            center: center as unknown as google.maps.LatLngLiteral,
+                            zoom: 12,
+                        },
+                    );
+                }
+            });
+        });
+    }, [orderedAddresses]);
 
-  return (
-    <div className="relative !grid grid-cols-[90vw] grid-rows-[150px_repeat(2,700px)] gap-[30px] justify-center !items-start w-auto max-w-full mx-auto [&_a]:text-white [&_a]:font-bold [&_a]:underline [&_h1]:text-white [&_h1>p]:text-white min-[1200px]:grid-cols-[auto_repeat(2,430px)] min-[1200px]:grid-rows-[auto] min-[1200px]:max-w-[80vw] min-[1200px]:!items-center min-[1129px]:max-[1200px]:pt-[20%] min-[800px]:max-[1128px]:pt-[30%] max-[600px]:pt-0">
-      <div className="text-white" style={{ minWidth: '255px' }}>
-        <h1>{Heading || name}</h1>
-        {richcontent?.map((content) =>
-          content?.children?.map((c) => <p key={id}>{c?.text}</p>),
-        )}
+    return (
+        <div className="relative !grid grid-cols-[90vw] grid-rows-[150px_repeat(2,700px)] gap-[30px] justify-center !items-start w-auto max-w-full mx-auto [&_a]:text-white [&_a]:font-bold [&_a]:underline [&_h1]:text-white [&_h1>p]:text-white min-[1200px]:grid-cols-[auto_repeat(2,430px)] min-[1200px]:grid-rows-[auto] min-[1200px]:max-w-[80vw] min-[1200px]:!items-center min-[1129px]:max-[1200px]:pt-[20%] min-[800px]:max-[1128px]:pt-[30%] max-[600px]:pt-0">
+            <div className="text-white" style={{ minWidth: '255px' }}>
+                <h1>{Heading || name}</h1>
+                {richcontent?.map((content) =>
+                    content?.children?.map((c) => <p key={id}>{c?.text}</p>),
+                )}
 
-        <button
-          className='cursor-pointer p-0 bg-none -translate-x-3'
-          onClick={(e) => {
-            e.preventDefault();
-            window.open('mailto:info@bluepathfinance.com');
-          }}
-        >
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src={emailLink} alt="Bluepath contact email" />
-        </button>
-      </div>
-      {orderedAddresses.map(({ _id, details, address }) => (
-        <div
-          key={_id}
-          onClick={() => setShowInfo(Boolean(_id))}
-          onMouseEnter={() => setShowInfo(Boolean(_id))}
-          onMouseLeave={() => setShowInfo(false)}
-          className="max-w-[430px] m-0 max-[600px]:max-w-[calc(100vw-20px)] max-[600px]:max-h-[calc(calc(calc(100%-20px)*330px)/430px)] [&_h3]:bg-blue [&_h3]:text-white [&_h3]:text-[2rem] [&_h3]:py-4 [&_h3]:px-6 [&_h3]:mb-0 [&_h3]:tracking-[1.3px] [&_h3]:uppercase [&_p]:text-white [&_p]:font-thin [&_p]:text-[1.3rem] [&_p]:p-[1.24rem] [&_p]:m-0 [&_p]:leading-[1.3]"
-        >
-          <h3>{splitByNewLines(String(address))}</h3>
-          <div
-            id={_id ?? undefined}
-            className="w-[430px] h-[330px] max-[600px]:max-w-full max-[600px]:max-h-[calc(calc(calc(100%-20px)*330px)/430px)]"
-          />
-          <div
-            className={classNames(
-              'opacity-0 transition-opacity duration-[240ms] ease-out bg-[var(--color-gray-1)] h-full',
-              { '!opacity-100': showInfo === Boolean(_id) },
-            )}
-          >
-            <p>{splitByNewLines(String(details))}</p>
-          </div>
+                <button
+                    className='cursor-pointer p-0 bg-none -translate-x-3'
+                    onClick={(e) => {
+                        e.preventDefault();
+                        window.open('mailto:info@bluepathfinance.com');
+                    }}
+                >
+                    {/* eslint-disable-next-line @next/next/no-img-element */}
+                    <img src={emailLink} alt="Bluepath contact email" />
+                </button>
+            </div>
+            {orderedAddresses.map(({ _id, details, address }) => (
+                <div
+                    key={_id}
+                    onClick={() => setShowInfo(Boolean(_id))}
+                    onMouseEnter={() => setShowInfo(Boolean(_id))}
+                    onMouseLeave={() => setShowInfo(false)}
+                    className="max-w-[430px] m-0 max-[600px]:max-w-[calc(100vw-20px)] max-[600px]:max-h-[calc(calc(calc(100%-20px)*330px)/430px)] [&_h3]:bg-blue [&_h3]:text-white [&_h3]:text-lg [&_h3]:py-4 [&_h3]:px-6 [&_h3]:mb-0 [&_h3]:tracking-[1.3px] [&_h3]:uppercase [&_p]:text-white [&_p]:font-thin [&_p]:text-[1.3rem] [&_p]:p-[1.24rem] [&_p]:m-0 [&_p]:leading-[1.3]"
+                >
+                    <h3>{splitByNewLines(String(address))}</h3>
+                    <div
+                        id={_id ?? undefined}
+                        className="w-[430px] h-[330px] max-[600px]:max-w-full max-[600px]:max-h-[calc(calc(calc(100%-20px)*330px)/430px)]"
+                    />
+                    <div
+                        className={classNames(
+                            'opacity-0 transition-opacity duration-[240ms] ease-out bg-[var(--color-gray-1)] h-full',
+                            { '!opacity-100': showInfo === Boolean(_id) },
+                        )}
+                    >
+                        <p>{splitByNewLines(String(details))}</p>
+                    </div>
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 };
